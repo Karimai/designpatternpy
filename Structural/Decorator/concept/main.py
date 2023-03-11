ead162191e2aa5e8fd40fcda32b0308b4c90ec37
Karim Moradi <kmoradi.ai@gmail.com>
2023-03-11 08:40:48 +0100

    simple concept code for decorator pattern

---
+++ Structural/Decorator/concept/main.py
@@ -0,0 +1,30 @@
+from abc import ABCMeta, abstractmethod
+
+
+class IFlower(metaclass=ABCMeta):
+    @staticmethod
+    @abstractmethod
+    def method():
+        "A method to implement"
+
+
+class Flower(IFlower):
+
+    def method(self):
+        return "Flower"
+
+
+class FlowerDecorator(IFlower):
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def method(self):
+        return f"A decorated {self.obj.method()}!"
+
+
+if __name__ == '__main__':
+    flower = Flower()
+    print(flower.method())
+
+    print(FlowerDecorator(flower).method())
