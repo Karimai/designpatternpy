f271e6a011a80f528245c3f96fc7199ce66fcdb6
Karim Moradi <kmoradi.ai@gmail.com>
2023-04-12 14:49:42 +0200

    add ocp

--- solid/open_close.py
+++ solid/open_close.py
@@ -1,6 +1,6 @@
 """
 Open for extension, close for modification
-It suggect when you add a new functionality, you will add by extension,
+It suggests when you add a new functionality, you will add by extension,
 not by modification.
 After you add and test a function, you can not modify it further
 """
@@ -30,22 +30,28 @@ class Product:
 
 class ProductFilter:
     # the basic implementation. It only has filter by color
-    def filter_by_color(self, products: List[Product], color: Color):
-        for p in products:
-            if p.color == color:
-                yield p
+    @staticmethod
+    def filter_by_color(prods: List[Product], color: Color):
+        # for p in products:
+        #     if p.color == color:
+        #         yield p
+        return filter(lambda pr: pr.color == color, prods)
 
     # according to a new feature request, a filter_by_size should be added
     # So, ok, we add another filter by size
-    def filter_by_size(self, products: List[Product], size: Size):
-        for p in products:
-            if p.size == size:
-                yield p
+    @staticmethod
+    def filter_by_size(prods: List[Product], size: Size):
+        # for p in products:
+        #     if p.size == size:
+        #         yield p
+        return filter(lambda pr: pr.size == size, prods)
 
-    def filter_by_color_size(self, products: List[Product], color: Color, size: Size):
-        for p in products:
-            if p.color == color and p.size == size:
-                yield p
+    @staticmethod
+    def filter_by_color_size(prods: List[Product], color: Color, size: Size):
+        # for p in products:
+        #     if p.color == color and p.size == size:
+        #         yield p
+        return filter(lambda pr: pr.color == color and pr.size == size, prods)
 
 
 apple = Product('Apple', Color.Green, Size.SMALL)
@@ -58,15 +64,18 @@ pf = ProductFilter()
 for p in pf.filter_by_color(products, Color.Green):
     print(f' - {p.name} is green!')
 
+for p in pf.filter_by_size(products, Size.LARGE):
+    print(f' - {p.name} is large!')
 
-################################## Appling OCP ###########
+# ################################# Applying OCP ###########
+print('############ OCP #############')
 
 
 class Specification:
     def is_satisfied(self, other):
         pass
 
-    # override and operator
+    # override the `and` operator!
     def __and__(self, other):
         return AndSpecification(self, other)
 
@@ -102,23 +111,25 @@ class SizeSpecification(Specification):
 
 class OCPFilter(Filter):
     def filter(self, items, spec):
-        for item in items:
-            if spec.is_satisfied(item):
-                yield item
+        # for item in items:
+        #     if spec.is_satisfied(item):
+        #         yield item
+        return filter(spec.is_satisfied, items)
 
 
 ocp_filter = OCPFilter()
 
+print('Green products:')
 green = ColorSpecification(Color.Green)
 for p in ocp_filter.filter(products, green):
-    print(f'  - {p.name} is green!')
+    print(f' - {p.name} is green!')
 
 print('Large products:')
 large = SizeSpecification(Size.LARGE)
 for p in ocp_filter.filter(products, large):
-    print(f'  - {p.name} is large!')
+    print(f' - {p.name} is large!')
 
-print('Large blue items')
+print('Large Blue items')
 blue = ColorSpecification(Color.Blue)
 large_blue = large & blue
 for p in ocp_filter.filter(products, large_blue):
