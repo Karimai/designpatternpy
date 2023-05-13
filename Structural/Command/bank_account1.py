b632117beaed7b5365154735c22f9ad8a324a69d
Karim Moradi <kmoradi.ai@gmail.com>
2023-05-13 17:16:37 +0200

    update command design pattern

---
+++ Structural/Command/bank_account1.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractmethod
+from __future__ import annotations
+
+
+class Command(ABC):
+    """
+    The Command Interface declares a method for executing a command.
+    """
+    @abstractmethod
+    def execute(self):
+        pass
+
+    @abstractmethod
+    def undo(self):
+        pass
+
+
+class BankAccount:
+    def __init__(self, balance: int):
+        self._balance = balance
+
+    def deposit(self, amount: int):
+        self._balance += amount
+        print(f"Deposit {amount}. Balance is now {self._balance}")
+
+    def withdraw(self, amount: int):
+        if self._balance >= amount:
+            self._balance -= amount
+            print(f"Withdraw {amount}. Balance is now {self._balance}")
+        else:
+            print(f"Insufficient funds. Balance is {self._balance}")
+
+    def transfer(self, amount: int, recipient: BankAccount):
+        if self._balance >= amount:
+            self._balance -= amount
+            recipient.deposit(amount)
+            print(f"Transferred {amount} to recipient. Balance is now {self._balance}")
+        else:
+            print(f"Insufficient funds. Balance is {self._balance}")
+
+
+class DepositCommand(Command):
+    """
+    A concrete command that deposits money into a bank account.
+    """
+    def __init__(self, account: BankAccount, amount: int):
+        self._account = account
+        self._amount = amount
+
+    def execute(self):
+        self._account.deposit(self._amount)
+
+    def undo(self):
+        self._account.withdraw(self._amount)
+
+
+class WithdrawCommand(Command):
+    """
+    A concrete command that deposits money into a bank account.
+    """
+    def __init__(self, account: BankAccount, amount: int):
+        self._account = account
+        self._amount = amount
+
+    def execute(self):
+        self._account.withdraw(self._amount)
+
+    def undo(self):
+        self._account.deposit(self._amount)
+
+
+class TransferCommand(Command):
+    def __init__(self, sender: BankAccount, recipient: BankAccount, amount: int):
+        self._sender = sender
+        self._recipient = recipient
+        self._amount = amount
+
+    def execute(self):
+        self._sender.transfer(self._amount, self._recipient)
+
+    def undo(self):
+        self._recipient.transfer(self._amount, self._sender)
+
+
+class BankSystem:
+    """
+    The Invoker class asks the command to carry out the request.
+    """
+    def __init__(self):
+        self._history = []
+
+    def execute_command(self, command: Command):
+        command.execute()
+        self._history.append(command)
+
+    def undo_last_command(self):
+        if self._history:
+            last_command = self._history.pop()
---
+++ Structural/Command/udemy_bank_account.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class BankAccount:
+    OVERDRAFTLIMIT = -500
+
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}, balance is {self.balance}")
+        return True
+
+    def withdraw(self, amount):
+        if self.balance - amount >= BankAccount.OVERDRAFTLIMIT:
+            self.balance -= amount
+            print(f"Withdraw {amount}, balance = {self.balance}")
+            return True
+        return False
+
+    def __str__(self):
+        return f"Balance = {self.balance}"
+
+
+class Command(ABC):
+    def invoke(self):
+        pass
+
+    def undo(self):
+        pass
+
+
+class BankAccountCommand(Command):
+    class Action(Enum):
+        DEPOSIT = 0
+        WITHDRAW = 1
+
+    def __init__(self, account: BankAccount, action: Action, amount: int):
+        self.success = None
+        self.account = account
+        self.action = action
+        self.amount = amount
+
+    def invoke(self):
+        if self.action == self.Action.DEPOSIT:
+            self.success = self.account.deposit(self.amount)
+        elif self.action == self.Action.WITHDRAW:
+            self.success = self.account.withdraw(self.amount)
+
+    def undo(self):
+        if not self.success:
+            return
+        if self.action == self.Action.DEPOSIT:
+            self.account.withdraw(self.amount)
+        elif self.action == self.Action.WITHDRAW:
+            self.account.deposit(self.amount)
+
+
+if __name__ == '__main__':
+    ba = BankAccount()
+    cmd = BankAccountCommand(
+        ba, BankAccountCommand.Action.DEPOSIT, 100
+    )
+
+    cmd.invoke()
+    print(f"After $100 deposit: {ba}")
+    cmd.undo()
+    print(f"After undo, deposit: {ba}")
+
+    illegal_cmd = BankAccountCommand(
+        ba, BankAccountCommand.Action.WITHDRAW, 1000
+    )
+    illegal_cmd.invoke()
+    print(f"Withdraw too much: {ba}")
+    illegal_cmd.undo()
+    print(f"after undo: ")
